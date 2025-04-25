0e1a5d449c7056c5af158a9f9918b4edb5971fae
HoangQuocDuy29 <quocduyhoang42@gmail.com>
2025-04-26 00:02:56 +0700

    v2

---
+++ Go-Tutorial/Data_Types/Boolean.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var b1 bool = true
+	var b2 = true
+	b3 := true
+	var b4 bool
+
+	fmt.Println(b1)
+	fmt.Println(b2)
+	fmt.Println(b3)
+	fmt.Println(b4)
+}
---
+++ Go-Tutorial/Data_Types/Interger.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 500
+	var b int = 100
+
+	fmt.Printf("Type : %T, value : %v \n", a, a)
+	fmt.Printf("Type : %T, value : %v", b, b)
+
+}
